575c1028e82b12ced16c0d9f9376161a9cdd7dfe
Fernando Zaiter <43217862+fzaiter@users.noreply.github.com>
2024-04-11 21:39:13 +0200

    Added progress bar and logos.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 function CopyToClipboard(text_id) {
     var Text = document.getElementById(text_id);
     navigator.clipboard.writeText(Text.value);
-    
     console.log("Email copied to clipboard");
+}
 
+// Progress bar - source: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_indicator
+function ProgressFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
 }
