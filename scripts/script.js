e74f42f2f7b0c9254bfdddb3a8212d84c53348b5
Fernando Zaiter <43217862+fzaiter@users.noreply.github.com>
2024-04-21 14:40:15 +0200

    Added al the real texts to the English page.

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
 function CopyToClipboard(text_id) {
-    var Text = document.getElementById(text_id);
-    navigator.clipboard.writeText(Text.value);
-    console.log("Email copied to clipboard");
+  var Text = document.getElementById(text_id);
+  navigator.clipboard.writeText(Text.value);
+  console.log("Email copied to clipboard");
 }
-
 // Progress bar - source: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_indicator
 function ProgressFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
