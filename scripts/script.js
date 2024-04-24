497042c16bd6cb87811942b2e5dd87767abcb4f6
Fernando Zaiter <43217862+fzaiter@users.noreply.github.com>
2024-04-24 22:02:35 +0200

    Amended the following issues, mainly: - Copy email and progress bar incorrect on HTML files after having changed the name. - Sitcky titles weren't working on wider screens. - Added blinking arrow to mobile screens. - Review of margins and paddings overall.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 function copyToClipboard(textId) {
   const textElement = document.getElementById(textId);
   navigator.clipboard.writeText(textElement.value);
-  console.log("Email copied to clipboard");
+  console.log("Text copied to clipboard");
 }
 
 // Progress bar - source: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_indicator
@@ -13,3 +13,12 @@ function progressFunction() {
   const scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = `${scrolled}%`;
 }
+
+window.onscroll = function () {
+  var arrow = document.getElementById("arrow");
+  if (window.pageYOffset > 0) {
+    arrow.style.display = "none";
+  } else {
+    arrow.style.display = "block";
+  }
+};
